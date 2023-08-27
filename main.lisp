e826c0fea8bd478b7df9e1e0a343118a00048b61
Gabriel Johnson <gabriel.johnson.pitp@gmail.com>
2023-08-27 15:19:01 -0700

    Make test class with run-test method

--- main.lisp
+++ main.lisp
@@ -1,2 +1,11 @@
 
 (in-package :trivial-unit-tests)
+
+(defclass test ()
+  ((name :reader test-name
+         :initarg :name)
+   (body :reader test-body
+         :initarg :body)))
+
+(defmethod run-test ((test test))
+  (funcall (test-body test)))
--- test.lisp
+++ test.lisp
@@ -1,2 +1,11 @@
 
 (in-package :trivial-unit-tests)
+
+(defun run-self-tests ()
+  (let* ((was-run nil)
+         (test (make-instance 'test
+                              :name 'example
+                              :body (lambda () (setq was-run t)))))
+    (assert (not was-run))
+    (run-test test)
+    (assert was-run)))
