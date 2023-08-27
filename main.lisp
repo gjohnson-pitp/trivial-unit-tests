190678492150f987891f9723d569ca4bb0a0dbf5
Gabriel Johnson <gabriel.johnson.pitp@gmail.com>
2023-08-27 15:24:12 -0700

    Add documentation strings to run-test and test

--- main.lisp
+++ main.lisp
@@ -1,11 +1,26 @@
 
 (in-package :trivial-unit-tests)
 
+(defgeneric run-test (test)
+  (:documentation "(run-test test)
+
+Runs the given test by calling the closure given as its body.
+
+SEE ALSO:
+test (class)"))
+
 (defclass test ()
   ((name :reader test-name
          :initarg :name)
    (body :reader test-body
-         :initarg :body)))
+         :initarg :body))
+  (:documentation "(make-instance 'test :name [a symbol] :body [a closure])
+
+A test object that takes a symbol as a name and a closure as a body. Run it with
+run-test.
+
+SEE ALSO:
+run-test (function)"))
 
 (defmethod run-test ((test test))
   (funcall (test-body test)))
