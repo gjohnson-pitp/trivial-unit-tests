184b049934f53b9f10bbd18ed13dbc00982eb2cf
Gabriel Johnson <gabriel.johnson.pitp@gmail.com>
2023-08-27 15:37:18 -0700

    Add/change documentation strings for on-fail, run-test, and test

--- main.lisp
+++ main.lisp
@@ -4,9 +4,24 @@
 (defgeneric run-test (test)
   (:documentation "(run-test test)
 
-Runs the given test by calling the closure given as its body.
+Runs the given test by calling the closure given as its body. The
+default method on the predefined test class catches any error thrown
+in the test's body and calls on-fail, passing it the test and the
+error.
 
 SEE ALSO:
+test (class)
+on-fail (function)"))
+
+(defgeneric on-fail (test error)
+  (:documentation "(on-fail test error)
+
+Hook called when a test fails. By default, called in run-test when the
+test body throws an error. It's passed the test object for the failed
+test and the error object thrown.
+
+SEE ALSO:
+run-test (function)
 test (class)"))
 
 (defclass test ()
@@ -20,7 +35,8 @@ A test object that takes a symbol as a name and a closure as a
 body. Run it with run-test.
 
 SEE ALSO:
-run-test (function)"))
+run-test (function)
+on-fail (function)"))
 
 (defmethod run-test ((test test))
   (handler-case (funcall (test-body test))
